085c34320ed89f10b188ba5d88fa04c9f2f2dfca
Peter Levi <peterlevi@peterlevi.com>
2015-08-03 02:41:15 +0300

    Some more cleanup

--- Slideshow.py
+++ Slideshow.py
@@ -10,9 +10,9 @@ import time
 IMAGE_TYPES = ('.jpg', '.png', '.bmp')
 INTERVAL = 4000
 FADE_TIME = INTERVAL / 3
-ZOOM_FACTOR = 1.2
+ZOOM_FACTOR = 1.25
 PAN_FACTOR = 60
-SAFETY_ZOOM = 1.05
+SAFETY_ZOOM = 1.01
 
 random.seed(time.time())
 logging.basicConfig()
@@ -40,6 +40,7 @@ class Slideshow:
         self.texture = Clutter.Texture.new()
         self.next_texture = None
         self.prev_texture = None
+        self.next_data = None
 
         def quit(*args):
             Clutter.main_quit()
@@ -51,26 +52,23 @@ class Slideshow:
         self.stage.connect('motion-event', quit)
 
         self.will_enlarge = random.choice((True, False))
-        Clutter.threads_add_timeout(GLib.PRIORITY_HIGH_IDLE, 300, self._after_show, None)
+        self.prepare_next_data()
+        Clutter.threads_add_timeout(GLib.PRIORITY_HIGH_IDLE, 300, self.next, None)
         self.stage.show()
 
         Clutter.main()
 
-    def _after_show(self, *args):
-        global SAFETY_ZOOM
-        SAFETY_ZOOM = 1.02 + 1.5 * PAN_FACTOR / min(self.stage.get_width(), self.stage.get_height())
-        self.next()
-
     def next(self, *args):
         try:
             self.will_enlarge = not self.will_enlarge
-            filename = os.path.join(self.folder, random.choice(self.files))
-            self.next_texture = self.create_texture(filename)
-            pan_zoom_params = self.calculate_zoom_pan_params(self.next_texture)
-            self.show_and_animate_texture(self.next_texture, *pan_zoom_params)
+            self.next_texture = self.create_texture()
+            target_size, target_position = self.initialize_pan_and_zoom(self.next_texture)
 
+            self.stage.add_actor(self.next_texture)
             self.toggle(self.texture, False)
             self.toggle(self.next_texture, True)
+
+            self.start_pan_and_zoom(self.next_texture, target_size, target_position)
 
             if self.prev_texture:
                 self.prev_texture.destroy()
@@ -78,12 +76,16 @@ class Slideshow:
             self.texture = self.next_texture
 
             Clutter.threads_add_timeout(GLib.PRIORITY_HIGH_IDLE, INTERVAL, self.next, None)
+            self.prepare_next_data()
         except:
             logging.exception('Oops:')
             Clutter.threads_add_timeout(GLib.PRIORITY_HIGH_IDLE, 100, self.next, None)
 
     def get_ratio_to_screen(self, texture):
         return max(self.stage.get_width() / texture.get_width(), self.stage.get_height() / texture.get_height())
+
+    def prepare_next_data(self):
+        self.next_data = self.load_image_data(os.path.join(self.folder, random.choice(self.files)))
 
     def load_image_data(self, filename):
         pixbuf = GdkPixbuf.Pixbuf.new_from_file(filename)
@@ -96,15 +98,15 @@ class Slideshow:
             4 if pixbuf.get_has_alpha() else 3,
             Clutter.TextureFlags.NONE)
 
-    def create_texture(self, filename):
-        data = self.load_image_data(filename)
+    def create_texture(self, filename=None):
+        data = self.next_data if not filename else self.load_image_data(filename)
         texture = Clutter.Texture.new()
         texture.set_from_rgb_data(*data)
         texture.set_opacity(0)
         texture.set_keep_aspect_ratio(True)
         return texture
 
-    def calculate_zoom_pan_params(self, texture):
+    def initialize_pan_and_zoom(self, texture):
         rand_pan = lambda: random.choice((-1, 1)) * (PAN_FACTOR + PAN_FACTOR * random.random())
         zoom_factor = ZOOM_FACTOR * (1 + 0.1 * random.random())
 
@@ -125,15 +127,13 @@ class Slideshow:
             initial_size, initial_position = big_size, big_position
             target_size, target_position = small_size, small_position
 
-        return initial_size, initial_position, target_size, target_position
-
-    def show_and_animate_texture(self, texture, initial_size, initial_position, target_size, target_position):
         # set initial size
         texture.set_size(*initial_size)
         texture.set_position(*initial_position)
 
-        self.stage.add_actor(texture)
+        return target_size, target_position
 
+    def start_pan_and_zoom(self, texture, target_size, target_position):
         # start animating to target size
         texture.save_easing_state()
         texture.set_easing_mode(Clutter.AnimationMode.LINEAR)
