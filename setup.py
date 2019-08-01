c3206a0d3c9caf294538ebcdb98afc89dc1d9651
Peter Levi <peterlevi@peterlevi.com>
2019-08-01 23:28:56 +0300

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name="variety-slideshow",
-    version="0.1",
+    version="0.1.1",
     license="GPL-3",
     author="Peter Levi",
     author_email="peterlevi@peterlevi.com",
